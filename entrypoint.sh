a82c3bc387d0ba4c2baf6ef7ae057d2485f3ef6a
saravahdatipour <verpiderp@googlemail.com>
2024-05-22 21:08:43 +0200

    streamlining making a realm for easier startup and testing

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start Keycloak server in the background
+/opt/keycloak/bin/kc.sh start-dev --hostname-strict=false --http-relative-path=/auth &
+
+# Wait for Keycloak to be ready
+echo "Waiting for Keycloak to start..."
+while ! (echo > /dev/tcp/localhost/8080) &> /dev/null; do
+  echo "Waiting for Keycloak server to be ready..."
+  sleep 5
+done
+
+# Run the setup script
+/opt/keycloak/setup-keycloak.sh
+
+# Keep the container running
+tail -f /dev/null
---
+++ setup-keycloak.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+##########
+#
+# This script sets up Keycloak using the Keycloak Config CLI and Keycloak Admin CLI.
+#
+#########
+
+trap 'exit' ERR
+
+echo " "
+echo " "
+echo "--------------- KEYCLOAK SETUP STARTING ----------------"
+echo " "
+echo " "
+
+BASEDIR=$(dirname "$0")
+
+runKeycloakConfigCli() {
+  echo ""
+  echo "--- Running Keycloak Config CLI"
+  echo ""
+
+  # run keycloak-config-cli
+  java -jar "${BASEDIR}"/keycloak-config-cli.jar \
+      --keycloak.url=http://localhost:8080/auth/ \
+      --keycloak.ssl-verify=false \
+      --keycloak.user="${KEYCLOAK_ADMIN}" \
+      --keycloak.password="${KEYCLOAK_ADMIN_PASSWORD}" \
+      --keycloak.availability-check.enabled=true \
+      --keycloak.availability-check.timeout=300s \
+      --import.var-substitution.enabled=true \
+      --import.managed.client=no-delete \
+      --import.managed.client-scope=no-delete \
+      --import.managed.client-scope-mapping=no-delete \
+      --import.files.locations="${BASEDIR}"/realm-config.json
+}
+
+runKeycloakCli() {
+  if [ "$KCADM" == "" ]; then
+      KCADM="/opt/keycloak/bin/kcadm.sh"
+      echo "Using $KCADM as the admin CLI."
+  fi
+
+  # login to admin console
+  ${KCADM} config credentials --server http://localhost:8080/auth/ --user "${KEYCLOAK_ADMIN}" --password "${KEYCLOAK_ADMIN_PASSWORD}" --realm master
+
+  # project specific configurations
+  # source "${BASEDIR}"/keycloak-cli-helpers.sh
+  # source "${BASEDIR}"/keycloak-cli-custom.sh
+}
+
+echo " "
+echo "----------------- KEYCLOAK CONFIG CLI ------------------"
+echo " "
+runKeycloakConfigCli
+
+echo " "
+echo "----------------- KEYCLOAK CLI ------------------"
+echo " "
+runKeycloakCli
+
+echo " "
+echo "--------------- KEYCLOAK SETUP FINISHED ----------------"
+echo " "
