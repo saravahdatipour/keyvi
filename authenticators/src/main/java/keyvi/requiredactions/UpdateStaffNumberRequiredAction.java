80a04135be3419666e7405fdb86e5f602ecccf02
Sara Vahdatipour <54913233+saravahdatipour@users.noreply.github.com>
2024-06-16 18:28:55 +0200

    sample required actions

---
+++ authenticators/src/main/java/keyvi/requiredactions/UpdateStaffNumberRequiredAction.java
@@ -0,0 +1,32 @@
+package keyvi.requiredactions;
+
+import org.keycloak.authentication.RequiredActionContext;
+import org.keycloak.authentication.RequiredActionProvider;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.FormMessage;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+public class UpdateStaffNumberRequiredAction implements RequiredActionProvider {
+
+    public static final String PROVIDER_ID = "usn-required-action";
+    private static final String UPDATE_STAFF_NUMBER_FORM = "update-staff-number.ftl";
+
+    @Override
+    public void evaluateTriggers(RequiredActionContext requiredActionContext) {
+    }
+
+    @Override
+    public void requiredActionChallenge(RequiredActionContext context) {
+    }
+
+    @Override
+    public void processAction(RequiredActionContext context) {
+    }
+
+    @Override
+    public void close() {
+    }
+}
