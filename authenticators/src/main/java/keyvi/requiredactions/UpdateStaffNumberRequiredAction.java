78177bce362c0b1b78e4d55d2c3723165ff031f2
saravahdatipour <verpiderp@googlemail.com>
2024-06-16 22:25:30 +0200

    required action challenge adjustments

--- authenticators/src/main/java/keyvi/requiredactions/UpdateStaffNumberRequiredAction.java
+++ authenticators/src/main/java/keyvi/requiredactions/UpdateStaffNumberRequiredAction.java
@@ -1,26 +1,36 @@
 package keyvi.requiredactions;
 
+import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
 import org.keycloak.authentication.RequiredActionContext;
 import org.keycloak.authentication.RequiredActionProvider;
-import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.UserModel;
-import org.keycloak.models.utils.FormMessage;
-import jakarta.ws.rs.core.MultivaluedMap;
-import jakarta.ws.rs.core.Response;
+import org.keycloak.sessions.AuthenticationSessionModel;
 
 public class UpdateStaffNumberRequiredAction implements RequiredActionProvider {
 
     public static final String PROVIDER_ID = "usn-required-action";
-    private static final String UPDATE_STAFF_NUMBER_FORM = "update-staff-number.ftl";
+    private static final Logger LOG = Logger.getLogger(UpdateStaffNumberRequiredAction.class);
 
     @Override
-    public void evaluateTriggers(RequiredActionContext requiredActionContext) {
+    public void evaluateTriggers(RequiredActionContext context) {
+        UserModel user = context.getUser();
+        AuthenticationSessionModel authSession = context.getAuthenticationSession();
+        if (isSocialLogin(authSession)) {
+            // Add your custom required action
+            user.addRequiredAction(PROVIDER_ID);
+            LOG.warnf("Social login was part of the session");
+        }
     }
 
     @Override
     public void requiredActionChallenge(RequiredActionContext context) {
-        Response challenge = context.form().createForm("required-action.ftl");
-        context.challenge(challenge);
+        if (isSocialLogin(context.getAuthenticationSession())) {
+            Response challenge = context.form().createForm("required-action.ftl");
+            context.challenge(challenge);
+        } else {
+            context.ignore();
+        }
     }
 
     @Override
@@ -29,5 +39,10 @@ public class UpdateStaffNumberRequiredAction implements RequiredActionProvider {
 
     @Override
     public void close() {
+    }
+
+    private boolean isSocialLogin(AuthenticationSessionModel authSession) {
+        String identityProvider = authSession.getAuthenticatedUser().getFederationLink();
+        return identityProvider != null && !identityProvider.isEmpty();
     }
 }
